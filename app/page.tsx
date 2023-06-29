731720208f94d0ab67010ffccc70ecac3f8a9aa6
Max Leiter <max.leiter@vercel.com>
2023-06-29 14:20:28 -0700

    Implement caching

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { OpenAIStream } from "ai";
 import { Configuration, OpenAIApi } from "openai-edge";
 import { Suspense } from "react";
 import { Tokens } from "ai/react";
+import { kv } from "@vercel/kv";
 
 export const runtime = "edge";
 
@@ -62,6 +63,39 @@ export default async function Page() {
 
 // We add a wrapper component to avoid suspending the entire page while the OpenAI request is being made
 async function Wrapper({ city, timezone }: { city: string; timezone: string }) {
+  const prompt =
+    "Act like as if you are a travel expert. Provide a list of 5 things to do in " +
+    city +
+    " in the " +
+    // The timezone helps the AI decide the correct state / location
+    timezone +
+    " timezone and start with 'here's a...'. Do NOT mention the timezone in your response.";
+
+  // See https://sdk.vercel.ai/docs/concepts/caching
+  const cached = (await kv.get(prompt)) as string | undefined;
+
+  if (cached) {
+    const chunks = cached.split(" ");
+    const stream = new ReadableStream({
+      async start(controller) {
+        for (const chunk of chunks) {
+          const bytes = new TextEncoder().encode(chunk + " ");
+          controller.enqueue(bytes);
+          await new Promise((r) =>
+            setTimeout(
+              r,
+              // get a random number between 10ms and 50ms to simulate a random delay
+              Math.floor(Math.random() * 40) + 10
+            )
+          );
+        }
+        controller.close();
+      },
+    });
+
+    return <Tokens stream={stream} />;
+  }
+
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     stream: true,
@@ -80,7 +114,12 @@ async function Wrapper({ city, timezone }: { city: string; timezone: string }) {
   });
 
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion) {
+      await kv.set(prompt, completion);
+      await kv.expire(prompt, 60 * 10);
+    },
+  });
 
   return <Tokens stream={stream} />;
 }
