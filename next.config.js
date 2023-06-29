0f3ef2fa4a243544b15ee9c9d3cd3cba70233b4d
Max Leiter <max.leiter@vercel.com>
2023-06-29 13:08:04 -0700

    Twitter bot detection

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  rewrites: {
+    source: "/:path*",
+    has: [
+      {
+        type: "header",
+        key: "User-Agent",
+        value: "Twitterbot",
+      },
+    ],
+    destination: "/twitter",
+  },
 };
 
 module.exports = nextConfig;
